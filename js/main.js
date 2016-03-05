8fffe55786dd9eb832b5432295ab4e555f875be4
Zameryth <zameryth+github@gmail.com>
2016-03-05 04:19:29 -0800

    Update

--- js/main.js
+++ js/main.js
@@ -19,12 +19,20 @@ function loadOptions() {
   var $hourOnes = document.getElementById('hourOnes');
   var $minTens = document.getElementById('minTens');
   var $minOnes = document.getElementById('minOnes');
+  var $topLeft = document.getElementById('topLeft');
+  var $topRight = document.getElementById('topRight');
+  var $botLeft = document.getElementById('botLeft');
+  var $botRight = document.getElementById('botRight');
 
   if (localStorage.weather) {
     $hourTens.value = localStorage.hourTens;
     $hourOnes.value = localStorage.hourOnes;
     $minTens.value = localStorage.minTens;
     $minOnes.value = localStorage.minOnes;
+    $topLeft.value = localStorage.topLeft;
+    $botLeft.value = localStorage.botLeft;
+    $topRight.value = localStorage.topRight;
+    $botRight.value = localStorage.botRight;
   }
 }
 
@@ -33,18 +41,30 @@ function getAndStoreConfigData() {
   var $hourOnes = document.getElementById('hourOnes');
   var $minTens = document.getElementById('minTens');
   var $minOnes = document.getElementById('minOnes');
+  var $topLeft = document.getElementById('topLeft');
+  var $topRight = document.getElementById('topRight');
+  var $botLeft = document.getElementById('botLeft');
+  var $botRight = document.getElementById('botRight');
 
   var options = {
     hourTens: $hourTens.value,
     hourOnes: $hourOnes.value,
     minTens: $minTens.value,
     minOnes: $minOnes.value,
+    topRight: $topRight.value,
+    botRight: $botRight.value,
+    topLeft: $topLeft.value,
+    botLeft: $botLeft.value,
   };
 
   localStorage.hourTens = options.hourTens;
   localStorage.hourOnes = options.hourOnes;
   localStorage.minTens = options.minTens;
   localStorage.minOnes = options.minOnes;
+  localStorage.topLeft = options.topLeft;
+  localStorage.botLeft = options.botLeft;
+  localStorage.topRight = options.topRight;
+  localStorage.botRight = options.botRight;
 
   console.log('Got options: ' + JSON.stringify(options));
   return options;
